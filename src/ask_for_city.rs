f680db7bb1c0402ef4dfb7f60ca205430a7ae900
Juan Marchetto <juanpatriciomarchetto@gmail.com>
2021-12-19 12:57:45 -0300

    close functionality

--- src/ask_for_city.rs
+++ src/ask_for_city.rs
@@ -3,13 +3,15 @@ use crate::helpers::get_number_from_user;
 use crate::show_weather::show_weather;
 use crate::structs::WeatherItem;
 
-pub fn ask_for_city(cities: Vec<String>, weather_data: Vec<WeatherItem>) {
+pub fn ask_for_city(cities: Vec<String>, weather_data: Vec<WeatherItem>) -> Option<()> {
     println!("{}", SELECT_CITY);
     for (index, city) in cities.iter().enumerate() {
         println!("[{}] {}", index, city);
     }
     let city = get_number_from_user(0, cities.len() as i32, CITY_ERROR_MESSAGE);
     if let Some(city_number) = city {
-        show_weather(cities, city_number, weather_data);
+        Some(show_weather(cities, city_number, weather_data))
+    } else {
+        None
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -12,27 +12,91 @@ mod structs;
 use ask_for_city::ask_for_city;
 use ask_for_day::ask_for_day;
 use ask_for_province::ask_for_province;
+use constants::{API_ERROR_MESSAGE, BYE};
 use get_data::get_data;
+use structs::WeatherItem;
 
-#[tokio::main]
-async fn main() -> Result<(), ExitFailure> {
-    // We get the weather forecast by asking to the user for the number of days
-    // and calling the get_data function with the number of days.
-    let weather_data = get_data(ask_for_day().unwrap()).await?;
+fn close() {
+    println!("{}", BYE);
+}
 
-    // We create a vector of strings with the names of the provinces.
+fn run_city(cities: Vec<String>, weather_data: Vec<WeatherItem>) {
+    if let Some(_) = ask_for_city(cities, weather_data) {
+        ()
+    } else {
+        close()
+    }
+}
+
+fn get_provinces(weather_data: &Vec<WeatherItem>) -> Vec<String> {
     let provinces: Vec<String> = weather_data.iter().fold(Vec::new(), |mut acc, item| {
         if !acc.contains(&item.province) {
             acc.push(item.province.clone());
         }
         acc
     });
+    provinces
+}
 
-    // We ask the user to select a province and we get a Vec with the cities of that province.
-    let cities = ask_for_province(provinces, &weather_data).unwrap();
+async fn run(days: i32) {
+    if let Ok(weather_data) = get_data(days).await {
+        let provinces = get_provinces(&weather_data);
+        let cities = ask_for_province(provinces, &weather_data);
+        if let Some(cities) = cities {
+            run_city(cities, weather_data);
+        } else {
+            close()
+        }
+    } else {
+        println!("{}", API_ERROR_MESSAGE);
+    }
+}
 
-    // We ask the user to select a city and we display the weather of that city.
-    ask_for_city(cities, weather_data);
-
+#[tokio::main]
+async fn main() -> Result<(), ExitFailure> {
+    // We ask to the user for the day
+    // If we got a day, we run the rest of the program
+    // Else we exit the program
+    let days = ask_for_day();
+    match days {
+        Some(days) => run(days).await,
+        _ => close(),
+    }
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use structs::WeatherDetails;
+    #[test]
+    fn test_handle_orders() {
+        let weather_data = vec![
+                WeatherItem {
+                    _id: "609d947e818e15902c74d831".to_string(),
+                    dist: 20.55,
+                    lid: 9539,
+                    fid: 9539,
+                    name: "Tunuyan".to_string(),
+                    province: "Mendoza".to_string(),
+                    lat: "-33.58210373".to_string(),
+                    lon: "-69.02268982".to_string(),
+                    zoom: "2".to_string(),
+                    updated: 1557349200,
+                    weather:
+                        WeatherDetails {
+                        day: 1,
+                        morning_temp: 10,
+                        morning_id: 2,
+                        morning_desc: "Cielo parcialmente nublado. Vientos leves del sector sur.".to_string(),
+                        afternoon_temp: 22,
+                        afternoon_id: 14,
+                        afternoon_desc: "Nubosidad variable. Probabilidad de tormentas aisladas. Vientos leves del sector sur.".to_string(),
+                }
+            }
+        ];
+        let provinces = get_provinces(&weather_data);
+        assert_eq!(provinces.len(), 1);
+        assert_eq!(provinces[0], "Mendoza");
+    }
 }
